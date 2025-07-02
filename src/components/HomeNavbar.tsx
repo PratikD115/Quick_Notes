96e10d86be69d24a8c1c4e0ef82eb134c25c4b9c
pratik dholariya <dholariyaprateek@gmail.com>
2025-07-02 23:22:03 +0530

    add the shadcn in project

--- src/components/HomeNavbar.tsx
+++ src/components/HomeNavbar.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 const HomeNavbar = () => {
   const router = useRouter();
-
   return (
     <nav className="w-full bg-white shadow-md px-6 py-4 flex justify-between items-center">
       {/* Left Section - Brand and Menu */}
@@ -19,18 +19,9 @@ const HomeNavbar = () => {
 
       {/* Right Section - Auth Buttons */}
       <div className="flex space-x-4">
-        <button
-          onClick={() => router.push("./login")}
-          className="text-blue-600 border border-blue-600 px-4 py-2 rounded-lg hover:bg-blue-50 transition"
-        >
-          Login
-        </button>
-        <button
-          onClick={() => router.push("./signup")}
-          className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition"
-        >
-          Sign Up
-        </button>
+        
+        <Button size={'lg'} variant='outline' onClick={() => router.push('./login')}>Log in</Button>
+        <Button size={'lg'} onClick={() => router.push('./signup')}>Sign Up</Button>
       </div>
     </nav>
   );
